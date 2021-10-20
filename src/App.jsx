d9f6760a029e8df7478369eacd824f91e20d8452
Jamund Ferguson <jamund@gmail.com>
2021-10-20 16:58:04 -0700

    basic e-book landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,17 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App-body">
+      <h1>How to be Great at Anything</h1>
+      <figure className="book">
+        <img height="300" src="/ebook.jpg" alt="How to be Great at Anything (Book Cover)" />
+        <figcaption>$13.99</figcaption>
+      </figure>
+      <form>
+        <button type="submit">Buy it Now</button>
+      </form>
     </div>
   )
 }
