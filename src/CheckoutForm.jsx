6cfc0a4328d25431a31d9a1494e8ead506ad5a21
Jamund Ferguson <jamund@gmail.com>
2021-10-21 12:48:01 -0700

    actually support checkout

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -2,8 +2,22 @@ import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 export function CheckoutForm() {
   return (
-    <PayPalScriptProvider options={{ "client-id": import.meta.CLIENT_ID }}>
-      <PayPalButtons style={{ layout: "horizontal" }} />
+    <PayPalScriptProvider
+      options={{ "client-id": import.meta.env.VITE_CLIENT_ID }}
+    >
+      <PayPalButtons
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                amount: {
+                  value: "13.99",
+                },
+              },
+            ],
+          });
+        }}
+      />
     </PayPalScriptProvider>
   );
 }
