71f5d2820d0b0b6b47c737f5fcb7c88cb000d277
Jamund Ferguson <jamund@gmail.com>
2021-10-22 13:49:04 -0700

    error cases

--- src/CheckoutForm.jsx
+++ src/CheckoutForm.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
+function Toast({ children }) {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    if (!children) return;
+    console.log("setting message to " + children);
+    setMessage(children);
+    setTimeout(() => {
+      console.log("clearing message");
+      setMessage("");
+    }, 2000);
+  }, [children]);
+  if (message) return <div className="Toast">{message}</div>;
+  return null;
+}
+
 export function CheckoutForm() {
+  const [message, setMessage] = useState("");
   return (
     <PayPalScriptProvider
       options={{ "client-id": import.meta.env.VITE_CLIENT_ID }}
     >
+      <Toast>{message}</Toast>
       <PayPalButtons
         createOrder={(data, actions) => {
           return actions.order.create({
@@ -16,6 +34,20 @@ export function CheckoutForm() {
               },
             ],
           });
+        }}
+        onApprove={async (data, actions) => {
+          throw new Error("do not capture");
+          const details = await actions.order.capture();
+          const name = details.payer.name.given_name;
+          setMessage("Transaction completed by " + name);
+        }}
+        onError={(error) => {
+          setMessage("Error: " + error.message);
+          console.error(error);
+        }}
+        onCancel={(data) => {
+          setMessage("Transaction canceled");
+          console.warn("Cancel", data);
         }}
       />
     </PayPalScriptProvider>
