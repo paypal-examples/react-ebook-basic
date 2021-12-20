805a45083159fcf2482302673ea7698c0c5f13ac
Jamund Ferguson <jamund@gmail.com>
2021-12-20 15:51:26 -0800

    simplify code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { CheckoutForm } from "./CheckoutForm";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import "./App.css";
 
 function App() {
   return (
     <div className="App-body">
-      <h1>How to be Great at Anything</h1>
+      <h1>How to be Great at Anything (E-Book)</h1>
       <img
         height="300"
         src="/ebook.jpg"
@@ -13,7 +13,28 @@ function App() {
       <p>
         <span className="book-price">$13.99</span>
       </p>
-      <CheckoutForm />
+      <PayPalScriptProvider
+        options={{ "client-id": import.meta.env.VITE_CLIENT_ID }}
+      >
+        <PayPalButtons
+          createOrder={(data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: "13.99",
+                  },
+                },
+              ],
+            });
+          }}
+          onApprove={async (data, actions) => {
+            const details = await actions.order.capture();
+            const name = details.payer.name.given_name;
+            alert("Transaction completed by " + name);
+          }}
+        />
+      </PayPalScriptProvider>
     </div>
   );
 }
